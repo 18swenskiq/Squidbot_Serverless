8a003b821e78c95ff9e67452587b0f70b5100fe6
Quinton Swenski <18swenskiq@gmail.com>
2023-04-30 23:19:01 -0400

    Update some typing stuff

--- src/discord_api/command.ts
+++ src/discord_api/command.ts
@@ -1,6 +1,7 @@
+import { Interaction } from './interaction';
 import { SlashCommandBuilder } from './slash_command_builder';
 
 export interface CommandDescription {
   data: SlashCommandBuilder;
-  execute: () => string;
+  execute: (interaction: Interaction) => string;
 }
