2b724ef0b97d00e2e1800360674491d9c1c17229
Quinton Swenski <18swenskiq@gmail.com>
2023-04-30 23:54:20 -0400

    Add some logging

--- src/commands/setTime.ts
+++ src/commands/setTime.ts
@@ -12,15 +12,18 @@ module.exports = {
       .setDescription('A city that you are in the time zone of. Try to select the biggest city you share a timezone with.')
       .setRequired(true)),
   execute (interaction: Interaction): string {
+    console.log('Step 1');
     const cityName = interaction.data.options[0].value.toLowerCase();
     const zones = getTimeZones({ includeUtc: true });
 
+    console.log('Step 2');
     const requestedZone = zones.find(z => z.mainCities.some(c => c.toLowerCase() === cityName));
 
     if (requestedZone === undefined) {
       return 'City name could not be found';
     }
 
+    console.log('Step 3');
     // If we're here, the city name was valid and we can go forward with writing the information
     return 'City name was valid! Functionality coming later';
   }
