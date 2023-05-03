c2ae7f0d2df5c554b1d25c6585810ad32f4794ce
Quinton Swenski <18swenskiq@gmail.com>
2023-05-03 00:39:13 -0400

    maybe

--- src/discord_api/miscEndpoints.ts
+++ src/discord_api/miscEndpoints.ts
@@ -4,14 +4,15 @@ import { GuildMember } from './guildMember';
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export abstract class MiscEndpoints {
   // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-  private static readonly baseUrl: string = `https://discord.com/api/v8/applications/${process.env.APP_ID}`;
+  private static readonly baseUrl: string = 'https://discord.com/api/v10'; /// applications/${process.env.APP_ID}`;
 
   public static async GetGuildMembers (guildId: string): Promise<GuildMember[]> {
     // TODO: Limitation, this currently only supports guilds with 250 members. Update to continually get them if needed;
     const url = this.baseUrl + `/guilds/${guildId}/members?limit=250`;
 
     try {
-      const response = await axios.get(url);
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      const response = await axios.get(url, { headers: { Authorization: `Bot ${process.env.BOT_TOKEN}` } });
       console.log('Success! Response data:', response.data);
       return response.data;
     } catch (exception) {
