1ff6b17373bbbc4cdfa90405bc3ebe6623420843
Quinton Swenski <18swenskiq@gmail.com>
2023-05-12 15:47:12 -0400

    updat

---
+++ handler_lambda/index.js
@@ -0,0 +1,61 @@
+const aws = require('aws-sdk');
+const nacl = require('tweetnacl');
+
+exports.handler = async(event) => {
+  const PUBLIC_KEY = process.env.PUBLIC_KEY;
+  const signature = event.headers['x-signature-ed25519'];
+  const timestamp = event.headers['x-signature-timestamp'];
+  const strBody = event.body; // should be string, for successful sign
+
+  if (PUBLIC_KEY === undefined) {
+    throw new Error('Public key was undefined!');
+  }
+
+  const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + strBody),
+    Buffer.from(signature, 'hex'),
+    Buffer.from(PUBLIC_KEY, 'hex')
+  );
+
+  if (!isVerified) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify('invalid request signature')
+    };
+  }
+  
+  const body = JSON.parse(strBody);
+  
+  switch (body.type) {
+	  case 1:
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ type: 1 })
+      };
+	  case 2:
+      const lambdaParams = {
+        FunctionName: 'SquidBot',
+        InvocationType: 'Event',
+        LogType: 'Tail',
+        Payload: Buffer.from('...') || strBody,
+      };
+
+      const lambda = new aws.Lambda({ region: 'us-east-2' });
+      lambda.invoke(lambdaParams, function(err, data) {
+        console.log("Invoking SquidBot");
+        if (err) {
+          console.log(err, err.stack);
+        } else {
+          console.log("Lambda triggered!");
+        }
+      });
+
+      return {
+        type: 5
+      };
+	  default:
+      return {
+        statusCode: 404
+      }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 /* eslint-disable no-case-declarations */
-import nacl from 'tweetnacl';
 import fs from 'fs';
 import path from 'path';
-import { Interaction, InteractionType } from './discord_api/interaction';
+import { Interaction } from './discord_api/interaction';
 import { CommandDescription } from './discord_api/command';
 import axios from 'axios';
 
@@ -10,27 +9,7 @@ exports.handler = async (event: { headers: Record<string, any>; body: string; })
   // Checking signature (requirement 1.)
   // Your public key can be found on your application in the Developer Portal
   console.log(event);
-  const PUBLIC_KEY = process.env.PUBLIC_KEY;
-  const signature: string = event.headers['x-signature-ed25519']
-  const timestamp: string = event.headers['x-signature-timestamp'];
   const strBody = event.body; // should be string, for successful sign
-
-  if (PUBLIC_KEY === undefined) {
-    throw new Error('Public key was undefined!');
-  }
-
-  const isVerified = nacl.sign.detached.verify(
-    Buffer.from(timestamp + strBody),
-    Buffer.from(signature, 'hex'),
-    Buffer.from(PUBLIC_KEY, 'hex')
-  );
-
-  if (!isVerified) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify('invalid request signature')
-    };
-  }
 
   console.log('Loading commands');
   const commands: CommandDescription[] = [];
@@ -49,43 +28,17 @@ exports.handler = async (event: { headers: Record<string, any>; body: string; })
   const body: Interaction = JSON.parse(strBody);
   console.log(`Body name: ${body.data.name}`);
 
-  switch (body.type) {
-    case InteractionType.PING:
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ type: 1 })
-      };
-    case InteractionType.APPLICATION_COMMAND:
-      // We need to defer the reply as this could take a bit of time
-      await createDeferral(body);
+  const chosenCommand = commands.find(c => c.data.name === body.data.name);
 
-      const chosenCommand = commands.find(c => c.data.name === body.data.name);
-
-      if (chosenCommand != null) {
-        const result = await chosenCommand.execute(body);
-        console.log('Returning result:', result);
-        // return JSON.stringify({ type: 4, data: { content: result } });
-        await sendCommandResponse(body, result);
-        return { statusCode: 200 };
-      }
-
-      break;
-
-    default:
-      console.log('returning 404 from unexpected body. Body is as follows:');
-      console.log(body);
-      return {
-        statusCode: 404 // If no handler implemented for Discord's request
-      }
+  if (chosenCommand != null) {
+    const result = await chosenCommand.execute(body);
+    console.log('Returning result:', result);
+    // return JSON.stringify({ type: 4, data: { content: result } });
+    await sendCommandResponse(body, result);
+    return { statusCode: 200 };
+  } else {
+    return { statusCode: 404 };
   }
-};
-
-async function createDeferral (originalInteraction: Interaction): Promise<Interaction> {
-  const res = await axios.post(`https://discord.com/api/v10/interactions/${originalInteraction.id}/${originalInteraction.token}/callback`, {
-    type: 5
-  });
-  console.log('Response from post: ', res);
-  return res.data;
 }
 
 async function sendCommandResponse (interaction: Interaction, message: string): Promise<void> {
@@ -94,14 +47,4 @@ async function sendCommandResponse (interaction: Interaction, message: string): 
     content: message
   });
   console.log('Response from editing message: ', res);
-
-  /*
-  const res = await axios.post(`https://discord.com/api/v10/interactions/${interaction.id}/${interaction.token}/callback`, {
-    type: 4,
-    data: {
-      content: message
-    }
-  });
-  console.log('Response from final send: ', res);
-  */
 }
