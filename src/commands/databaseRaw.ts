ff9d8e31852be7a903799ad2045b8a1827a54930
Quinton Swenski <18swenskiq@gmail.com>
2023-05-26 22:03:25 -0400

    fix bot for now

--- src/commands/databaseRaw.ts
+++ src/commands/databaseRaw.ts
@@ -1,7 +1,6 @@
 import { type CommandDescription } from '../discord_api/command'
 import { type Interaction } from '../discord_api/interaction'
 import { SlashCommandBuilder } from '../discord_api/slash_command_builder'
-import { Database2 } from '../util/database2';
 
 const userList = ['66318815247466496'];
 
@@ -15,7 +14,10 @@ module.exports = {
       return 'You do not have permissions to run this command. Requires: [BEING SQUIDSKI]';
     }
 
+    /*
     const result = await Database2.Test();
     return result;
+    */
+    return 'vibeo gane';
   }
 } as CommandDescription;
