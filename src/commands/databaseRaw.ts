308246080d8cea4c8c4b16d93d9d099ca1a274f1
Quinton Swenski <18swenskiq@gmail.com>
2023-05-26 23:47:36 -0400

    add thing

--- src/commands/databaseRaw.ts
+++ src/commands/databaseRaw.ts
@@ -1,6 +1,7 @@
 import { type CommandDescription } from '../discord_api/command'
 import { type Interaction } from '../discord_api/interaction'
 import { SlashCommandBuilder } from '../discord_api/slash_command_builder'
+import { Database2 } from '../util/database2';
 
 const userList = ['66318815247466496'];
 
@@ -8,16 +9,17 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('database_raw')
     .setDescription('[SQUIDSKI ONLY] Enables ability to run raw SQL')
+    .addStringOption(option => option
+      .setName('sql')
+      .setDescription('The sql to execute')
+      .setRequired(true))
     .setAllowedUsersOnly(userList),
   async execute (interaction: Interaction): Promise<string> {
     if (!userList.includes(interaction.member.user.id)) {
       return 'You do not have permissions to run this command. Requires: [BEING SQUIDSKI]';
     }
 
-    /*
-    const result = await Database2.Test();
+    const result = await Database2.PassRawSQL(interaction.data.options[0].value);
     return result;
-    */
-    return 'vibeo gane';
   }
 } as CommandDescription;
