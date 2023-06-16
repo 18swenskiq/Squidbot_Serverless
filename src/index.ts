b59c30b73a010f34c7101cfdb440c53a17a07fa7
Quinton Swenski <18swenskiq@gmail.com>
2023-06-16 17:13:32 -0400

    f

--- src/index.ts
+++ src/index.ts
@@ -87,6 +87,8 @@ async function sendCommandResponse (interaction: Interaction, result: CommandRes
       // const newInteraction: Interaction = originalInteractionReply.data.interaction;
 
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      await axios.post(`https://discord.com/api/v10/webhooks/${process.env.APP_ID}/${interaction.token}`, { content: 'User is selecting roles!' });
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       const followUpResult = await axios.post(`https://discord.com/api/v10/webhooks/${process.env.APP_ID}/${interaction.token}`, body);
       console.log('Followup result: ', followUpResult);
 
