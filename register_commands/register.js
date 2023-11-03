1316d9970a968a11d7149f58236aa6918cee0111
Quinton Swenski <18swenskiq@gmail.com>
2023-11-03 03:02:59 -0400

    update register

--- register_commands/register.js
+++ register_commands/register.js
@@ -1,68 +1,72 @@
-const axios = require('axios').default;
-var fs = require('fs');
-var path = require('path');
+const axios = require("axios").default;
+var fs = require("fs");
+var path = require("path");
 
-let url = `https://discord.com/api/v8/applications/${process.env.APP_ID}/commands`
+let url = `https://discord.com/api/v8/applications/${process.env.APP_ID}/commands`;
 
 const headers = {
-  "Authorization": `Bot ${process.env.BOT_TOKEN}`,
-  "Content-Type": "application/json"
-}
+  Authorization: `Bot ${process.env.BOT_TOKEN}`,
+  "Content-Type": "application/json",
+};
 
 // Load Commands
 let commands = [];
 let commandsPath = path.resolve(__dirname, "../dist/commands/");
-let commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+let commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 console.log(`Loading ${commandFiles.length} commands...`);
 for (const file of commandFiles) {
-      let command = require(`${commandsPath}/${file}`);
-      commands.push(command);
- }
+  let command = require(`${commandsPath}/${file}`);
+  commands.push(command);
+}
 
 let command_data = [];
 
 for (var command of commands) {
-	let localCommand = { 
-		"name": command.data.name,
-		"description": command.data.description,
-		"type": 1
-	};
+  let localCommand = {
+    name: command.data.name,
+    description: command.data.description,
+    type: 1,
+  };
 
-	if (command.data.options.length > 0) {
-		localCommand['options'] = [];
-		for (var option of command.data.options) {
-			let localOption = {
-				"name": option.name,
-				"description": option.description,
-				"type": option.type,
-				"required": option.required
-			}
+  if (command.data.options.length > 0) {
+    localCommand["options"] = [];
+    for (var option of command.data.options) {
+      let localOption = {
+        name: option.name,
+        description: option.description,
+        type: option.type,
+        required: option.required,
+      };
 
-			if (option['choices']) {
-				localOption['choices'] = [];
-				for (var choice of option.choices) {
-					localOption.choices.push({
-						"name": choice.name,
-						"value": choice.value
-					});
-				}
-			}
+      if (option["choices"]) {
+        localOption["choices"] = [];
+        for (var choice of option.choices) {
+          localOption.choices.push({
+            name: choice.name,
+            value: choice.value,
+          });
+        }
+      }
 
-			localCommand.options.push(localOption);
-		}
-	}
+      localCommand.options.push(localOption);
+    }
+  }
 
-	if (command.data.default_member_permissions.length > 0) {
-		if (command.data.default_member_permissions.length > 1) {
-			localCommand['default_member_permissions'] = command.data.default_member_permissions.reduce(function(a, b) {
-				return a | b;
-			});
-		} else {
-			localCommand['default_member_permissions'] = command.data.default_member_permissions[0];
-		}
-	}
+  if (command.data.default_member_permissions.length > 0) {
+    if (command.data.default_member_permissions.length > 1) {
+      localCommand["default_member_permissions"] =
+        command.data.default_member_permissions.reduce(function (a, b) {
+          return a | b;
+        });
+    } else {
+      localCommand["default_member_permissions"] =
+        command.data.default_member_permissions[0];
+    }
+  }
 
-	command_data.push(localCommand);
+  command_data.push(localCommand);
 }
 
 console.log("commands:");
@@ -70,6 +74,10 @@ console.log(command_data);
 
 console.log(`Attempting to register ${command_data.length} commands...`);
 
-axios.put(url, JSON.stringify(command_data), {
-  headers: headers,
-})
+try {
+  let res = axios.put(url, JSON.stringify(command_data), {
+    headers: headers,
+  });
+} catch (error) {
+  console.log(util.inspect(error.response, false, null));
+}
