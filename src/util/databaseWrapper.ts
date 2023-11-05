0cb983d6c840e714c46bab28edb864997318b697
Quinton Swenski <18swenskiq@gmail.com>
2023-11-05 02:05:43 -0500

    abwaweabg

--- src/util/databaseWrapper.ts
+++ src/util/databaseWrapper.ts
@@ -226,10 +226,14 @@ export abstract class DatabaseWrapper {
         console.log(objects);
 
         objects = objects.filter((o) => o.endsWith('bson'));
+        console.log('filter 1');
+        console.log(objects);
         objects = objects.map((o) => o.split('/')[2].replace('.bson', ''));
+        console.log('filter 2');
+        console.log(objects);
         objects = objects.filter((o) => o.includes(guildId));
 
-        console.log('Objects');
+        console.log('Objects - filter 3');
         console.log(objects);
 
         const retObj: Record<Snowflake, DB_PlaytestRequest> = {};
