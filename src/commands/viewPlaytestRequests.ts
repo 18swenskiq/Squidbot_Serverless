8a1c16c669569013e4e24fa7e1ab287f1a9cf4fb
Quinton Swenski <18swenskiq@gmail.com>
2023-11-07 13:50:43 -0500

    this is the one

--- src/commands/viewPlaytestRequests.ts
+++ src/commands/viewPlaytestRequests.ts
@@ -59,6 +59,9 @@ module.exports = {
                 Number(hhmm[1])
             );
 
+            const easternOffset = getOffset('US/Eastern');
+            composedDate.setMinutes(composedDate.getMinutes() + easternOffset);
+
             embed.fields?.push({
                 //name: `${value.mapName} by ${user.username} - (${value.requestDate} ${value.requestTime})`,
                 name: `${value.mapName} by ${user.username} - (<t:${composedDate.getTime() / 1000}:f>)`,
@@ -76,3 +79,24 @@ module.exports = {
         }
     },
 } as CommandDescription;
+
+const getOffset = (timeZone: any) => {
+    const timeZoneFormat = Intl.DateTimeFormat('ia', {
+        timeZoneName: 'short',
+        timeZone,
+    });
+    const timeZoneParts = timeZoneFormat.formatToParts();
+    const timeZoneName = timeZoneParts.find((i) => i.type === 'timeZoneName')!.value;
+    const offset = timeZoneName.slice(3);
+    if (!offset) return 0;
+
+    const matchData = offset.match(/([+-])(\d+)(?::(\d+))?/);
+    if (!matchData) throw `cannot parse timezone name: ${timeZoneName}`;
+
+    const [, sign, hour, minute] = matchData;
+    let result = parseInt(hour) * 60;
+    if (sign === '+') result *= -1;
+    if (minute) result += parseInt(minute);
+
+    return result;
+};
