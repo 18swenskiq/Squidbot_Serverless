8357ff93924fa682d38933b0376217a3ca9751d5
Quinton Swenski <18swenskiq@gmail.com>
2024-04-27 18:00:03 -0400

    fix

--- register_commands/register.js
+++ register_commands/register.js
@@ -41,7 +41,7 @@ files.forEach((file) => {
     .replace("var __esDecorate = ", "/*\nvar __esDecorate = ")
     .replace("var __runInitializers", "*/\nvar __runInitializers")
     .replace("_id_decorators = [", "//_id_decorators = [")
-    .replace('"use strict";', "");
+    .replace("let //_id_decorators", "let _id_decorators");
   fileText = fileText.replace("__esDecorate", "//__esDecorate");
   console.log(`New File text: ${fileText}`);
   console.log("-------------------");
