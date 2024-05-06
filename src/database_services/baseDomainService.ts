08daf5d88cc2fa23bb3ab70556eb0ebff651fc69
Quinton Swenski <18swenskiq@gmail.com>
2024-05-06 02:31:14 -0400

    fax

--- src/database_services/baseDomainService.ts
+++ src/database_services/baseDomainService.ts
@@ -3,10 +3,8 @@ import { AppDataSource } from '../util/data-source';
 
 export const BaseDomainService = <T extends ObjectLiteral>(entity: EntityTarget<T>) => {
     abstract class DomainServiceMixin {
-        protected repository: Repository<T>;
-
-        constructor() {
-            AppDataSource.getRepository(entity);
+        get repository(): Repository<T> {
+            return AppDataSource.getRepository(entity);
         }
     }
 
